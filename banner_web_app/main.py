79baeee98438191deca663f5645be5a9edaa5270
Prakarsh Upmanyu <prakarsh@prakarshs-air.home>
2021-10-08 09:11:59 -0400

    Adding Redis to docker to try caching
    Signed-off-by: Prakarsh Upmanyu <prakarsh@prakarshs-air.home>

--- banner_web_app/main.py
+++ banner_web_app/main.py
@@ -1,3 +1,4 @@
+import redis
 from flask import Flask, render_template, request, url_for, redirect
 import mysql.connector
 
@@ -6,6 +7,7 @@ db_connection = mysql.connector.connect(host="mysql", user="user", password="pas
 db_cursor = db_connection.cursor()
 
 app = Flask(__name__)
+redis = redis.Redis(host='redis', port=6379, db=0)
 
 
 @app.route('/')
@@ -32,3 +34,18 @@ def get_specific_campaign(campaign_id):
     num_converted_banners = result[0][0]
 
     return str(result[0][0])
+
+
+
+# @app.route('/visitor')
+# def visitor():
+#     redis.incr('visitor')
+#     visitor_num = redis.get('visitor').decode("utf-8")
+#     return "Visitor: %s" % visitor_num
+#
+#
+# @app.route('/visitor/reset')
+# def reset_visitor():
+#     redis.set('visitor', 0)
+#     visitor_num = redis.get('visitor').decode("utf-8")
+#     return "Visitor is reset to %s" % visitor_num
